f52996ec1420ac2a3ec42ed5b2a5b8106654c38c
Adam Gardner <adam.gardner@magicmemories.com>
2018-09-17 14:41:19 -1000

    Add some IPSec support

--- lib/puppet/util/network_device/mikrotik/facts.rb
+++ lib/puppet/util/network_device/mikrotik/facts.rb
@@ -26,6 +26,45 @@ class Puppet::Util::NetworkDevice::Mikrotik::Facts
       facts[new_key] = v
     end
     
-    facts
+    facts.merge(get_other_facts)
+  end
+
+  def get_other_facts
+    {
+      'ec2_metadata' => ec2_metadata,
+      'osfamily' => 'Mikrotik',
+      'os' => {
+        'family' => 'Mikrotik',
+        'name'   => 'RouterOS'
+      }
+    }
+  end
+
+  def ec2_metadata
+    {
+      'instance-id' => ec2('instance-id'),
+      'instance-type' => ec2('instance-type'),
+      'local-ipv4' => ec2('local-ipv4'),
+      'mac' => ec2('mac'),
+      'public-ipv4' => ec2('public-ipv4'),
+      'placement' => {
+         'availability-zone' => ec2('placement/availability-zone'),
+      },
+    }
+  end
+
+  def ec2(path)
+    params = {
+      'url' => "http://169.254.169.254/latest/meta-data/#{path}",
+      'mode' => 'http',
+      'output' => 'user', 
+    }
+    p_array = params.map { |k,v| "=#{k}=#{v}" }
+    reply = connection.get_reply('/tools/fetch',*p_array)
+    if result = reply.find_sentence('data')
+      result['data']
+    else
+      nil
+    end
   end
 end
