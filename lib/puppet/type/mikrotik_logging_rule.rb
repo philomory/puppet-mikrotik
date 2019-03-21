5a6579b5d802921c0e319036ac23f6f38a88e1a2
Adam Gardner <adam.gardner@magicmemories.com>
2019-03-21 12:37:53 -1000

    I really hate the mikrotik_logging_rule resource

--- lib/puppet/type/mikrotik_logging_rule.rb
+++ lib/puppet/type/mikrotik_logging_rule.rb
@@ -22,7 +22,7 @@ Puppet::Type.newtype(:mikrotik_logging_rule) do
       test_is     =      is.kind_of?(Array) ? is      :      is.split(',')
       test_should = @should.kind_of?(Array) ? @should : @should.split(',')
       result = (test_is.sort == test_should.sort)
-      Puppet.debug("Comparing #{test_is.sort.inspect} to #{test_should.sort.inspect}: #{result}"
+      Puppet.debug("Comparing #{test_is.sort.inspect} to #{test_should.sort.inspect}: #{result}")
       result
     end    
   end
